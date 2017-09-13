9872cb794e3c59ac778fcc1ae7ced94a6ed3fd43
Yuchen Qiu <yuchen.qiu@ucdconnect.ie>
2017-09-13 18:14:44 +0100

    using Helper to sort

---
+++ src/Sorting/Sort_using_SortTestHelper.java
@@ -0,0 +1,42 @@
+package Sorting;
+
+public class Sort_using_SortTestHelper
+{
+	private Sort_using_SortTestHelper()
+	{
+	}
+
+	public static <T extends Comparable<T>> void sort(T[] arr)
+	{
+		int n = arr.length;
+		for (int i = 0; i < n; i++)
+		{
+			int minIndex = i;
+			for (int j = i + 1; j < n; j++)
+				// use compareTo compare 2 Comparable
+				if (arr[j].compareTo(arr[minIndex]) < 0)
+					minIndex = j;
+
+			swap(arr, i, minIndex);
+
+		}
+	}
+
+	private static void swap(Object[] arr, int i, int j)
+	{
+		Object t = arr[i];
+		arr[i] = arr[j];
+		arr[j] = t;
+	}
+
+	public static void main(String[] args)
+	{
+		int N = 20000;
+		Integer[] arr = SortTestHelper.generateRandomArray(N, 0, 100000);
+		Sort_using_SortTestHelper.sort(arr);
+		SortTestHelper.printArray(arr);
+
+		return;
+	}
+
+}
