ab6e3a7dbf37dba2b87277d06718cc35cfb44485
Yuchen Qiu <yuchen.qiu@ucdconnect.ie>
2017-09-18 13:19:47 +0100

    subclass of account

---
+++ src/P8OOP/SavingsAccount.java
@@ -0,0 +1,30 @@
+package P8OOP;
+
+import P6OOP.Account;
+
+public class SavingsAccount extends Account
+{
+	public SavingsAccount()
+	{
+		super();
+	}
+
+	public SavingsAccount(int id, double balance)
+	{
+		super(id, balance);
+	}
+
+	@Override
+	public void withdraw(double ammount)
+	{
+		double balance = getbalance();
+		if (balance < ammount)
+		{
+			System.out.println("Your current balance is" + balance + " You have attempted to withdraw " + ammount
+					+ ".This transaction cannot be completed. Your balance is unchanged.");
+		} else
+		{
+			super.withdraw(ammount);
+		}
+	}
+}
